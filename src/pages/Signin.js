9c1719de5cf99d2dcddd7cb3f14114a3d74e40dd
Yoali Ambrosio <somerhalder96c@gmail.com>
2024-08-09 10:27:54 -0600

    Formularios inicio y registro

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -7,18 +7,18 @@ export default function Signin() {
     <div className="page-sign">
       <Card className="card-sign">
         <Card.Header>
-          <Link to="/" className="header-logo mb-4">FUT7</Link>
+          <Link to="/" className="header-logo mb-2">FUT7</Link>
           <Card.Title>Inicio de sesión</Card.Title>
-          <Card.Text>¡Bienvenido! Por favor, ingresa los datos correctos para continuar.</Card.Text>
+          <Card.Text>Asegúrate de ingresar los datos correctamente.</Card.Text>
         </Card.Header>
         <Card.Body>
           <Form method="get" action="/dashboard/helpdesk">
             <div className="mb-4">
-              <Form.Label >Correo electrónico</Form.Label>
+              <Form.Label ><i className="ri-mail-fill"></i> Correo electrónico</Form.Label>
               <Form.Control type="text" placeholder="Ingrese Correo Electrónico" value="me@themepixels.com" />
             </div>
             <div className="mb-4">
-              <Form.Label className="d-flex justify-content-between">Contraseña {/* <Link to="">Forgot password?</Link>*/}</Form.Label>
+              <Form.Label><i className="ri-lock-password-fill"></i> Contraseña {/* <Link to="">Forgot password?</Link>*/}</Form.Label>
               <Form.Control type="password" placeholder="Ingrese su constraseña" value="password123" />
             </div>
             <Button type="submit" variant="primary" className="btn-sign">Ingresar</Button>
@@ -32,7 +32,7 @@ export default function Signin() {
           </Form>
         </Card.Body>
         <Card.Footer>
-        <div className="divider">¿Aún sin una cuenta? <Link to="/pages/signup"><span></span> Registrarse</Link></div>          
+        <div className="divider">¿Aún sin una cuenta? <Link to="/pages/signup">&nbsp; Registrarse</Link></div>          
         </Card.Footer>
       </Card>
     </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,24 +7,28 @@ export default function Signup() {
     <div className="page-sign">
       <Card className="card-sign">
         <Card.Header>
-          <Link to="/" className="header-logo mb-4">FUT7</Link>
-          <Card.Title>Registro</Card.Title>
-          <Card.Text>¡Bienvenido a la plataforma de FUT7!, por favor ingrese los datos correctos.</Card.Text>
+          <Link to="/" className="header-logo mb-2">FUT7</Link>
+          <Card.Title>¡Bienvenido!</Card.Title>
+          <Card.Text>Regístrate a la plataforma FUT7</Card.Text> {/**, por favor ingrese los datos correctos. */}
         </Card.Header>
         <Card.Body>
-        <div className="mb-3">
-            <Form.Label>Nombre</Form.Label>
+          <div className="mb-2">
+            <Form.Label><i className="ri-user-fill"></i> Nombre</Form.Label>
             <Form.Control type="text" placeholder="Ingrese su nombre completo" />
           </div>
-          <div className="mb-3">
-            <Form.Label>Correo</Form.Label>
-            <Form.Control type="text" placeholder="Ingrese su Correo Electrónico" />
+          <div className="mb-2">
+            <Form.Label><i className="ri-phone-fill"></i> Teléfono</Form.Label>
+            <Form.Control type="text" placeholder="Ingrese su Número de Teléfono"/>
           </div>
-          <div className="mb-3">
-            <Form.Label>Contraseña</Form.Label>
+          <div className="mb-2">
+            <Form.Label><i className="ri-mail-fill"></i> Correo</Form.Label>
+            <Form.Control type="mail" placeholder="Ingrese su Correo Electrónico" />
+          </div>
+          <div className="mb-2">
+            <Form.Label><i className="ri-lock-password-fill"></i> Contraseña</Form.Label>
             <Form.Control type="password" placeholder="Genere una contraseña" />
           </div>          
-          <div className="mb-4">
+          <div className="mb-3">
             <small>Al crear una cuenta, usted está aceptando nuestros  <Link to="">Términos y condiciones</Link>.</small>
           </div>
           <Button variant="primary" className="btn-sign">Crear una cuenta</Button>
@@ -37,7 +41,7 @@ export default function Signup() {
           </Row> */}
         </Card.Body>
         <Card.Footer>
-        <div className="divider">¿Ya tiene una cuenta? <Link to="/pages/signin">Iniciar sesión</Link></div>          
+        <div className="divider">¿Ya tiene una cuenta?&nbsp; <Link to="/pages/signin">Iniciar sesión</Link></div>          
         </Card.Footer>
       </Card>
     </div>
